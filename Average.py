eb86bb3fc0ff7e135be3a0103a508263eda1a767
Srimaancodingal <srimaan.kv3@gmail.com>
2026-08-30 10:23:48 +0530

    Average.py

---
+++ Average.py
@@ -0,0 +1,10 @@
+Tree1 = 98
+Tree2 = 94
+Tree3 = 41
+Tree4 = 96
+Tree5 = 11
+total = Tree1 + Tree2 + Tree3 + Tree4 + Tree5
+average = total / 5
+print ("Total of all trees ", total)
+print ("Average of all trees ", int (average))
+
