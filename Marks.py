904881fb7b9ebf91260176872c522096d704098b
Srimaancodingal <srimaan.kv3@gmail.com>
2026-08-30 10:53:39 +0530

    Marks.py

---
+++ Marks.py
@@ -0,0 +1,7 @@
+English= int (input ("Enter the marks of English: "))
+Math= int (input ("Enter the marks of Math: "))
+Science= int (input ("Enter the marks of Science: "))
+Total= English + Math + Science
+print ("The total marks is: ", Total)
+percentage = (Total / 300) * 10
+print ("The percentage is: ", percentage)
