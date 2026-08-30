d846b51a4b56d0947d6e3690ecc3d47c1e365140
Srimaancodingal <srimaan.kv3@gmail.com>
2026-08-30 15:31:52 +0530

    Square root Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,7 +1,3 @@
-name= "CODINGAL"
-revStr = (name [:: -1])
-print (name [2:])
-print (name [1: 6])
-revstr = name[::-1]
-print ("Original Text", name )
-print ("reversed ", revstr)
+number= int (input("Enter a number: "))
+square_root = number ** 0.5
+print("The square root of", number, "is", square_root)
--- Marks.py
+++ Marks.py
@@ -1,7 +1,11 @@
 English= int (input ("Enter the marks of English: "))
 Math= int (input ("Enter the marks of Math: "))
 Science= int (input ("Enter the marks of Science: "))
-Total= English + Math + Science
+Social= int (input ("Enter the marks of Social: "))
+Telugu= int (input ("Enter the marks of Telugu: "))
+Hindi= int (input ("Enter the marks of Hindi: "))
+ComputerScience= int (input ("Enter the marks of Computer Science: "))
+Total= English + Math + Science + Social + Telugu + Hindi + ComputerScience
 print ("The total marks is: ", Total)
-percentage = (Total / 300) * 10
+percentage = (Total / 700) * 100
 print ("The percentage is: ", percentage)
