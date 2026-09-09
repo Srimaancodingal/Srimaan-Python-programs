df8485acaf05226ae12ce3d891f53cf5218e3931
Srimaancodingal <srimaan.kv3@gmail.com>
2026-09-09 18:16:52 +0530

    Age.py

---
+++ Abcd.py/Ageelegibility.py
@@ -0,0 +1,7 @@
+age= int(input("please enter your age : "))
+if age<=10:
+    print ("You are not allowed to the class")
+elif age>10<20:
+    print ("You are allowed for the class")
+else: 
+    print ("You are not allowed for the class")
