d2eac20b6194ce09f186935b4c194302741afb9e
Srimaancodingal <srimaan.kv3@gmail.com>
2026-09-13 10:42:45 +0530

    Name.py

---
+++ Name.py
@@ -0,0 +1,6 @@
+name=input("Enter the name : ")
+revname= ""
+for i in name:
+    revname= i + revname
+print ("Original name", name)
+print ("Reversed name", revname)
--- sUM OF WHOLE NUMBERS.py
+++
@@ -1,5 +0,0 @@
-num= int (input("Enter your number : " ))
-sum= 0
-for i in range(num+1):
-   sum= sum + i
-print ("Sum of whole numbers", num, "is", sum)
