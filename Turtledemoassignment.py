616645518c7731b46266ffd7000bd79c0dc63ff6
Srimaancodingal <srimaan.kv3@gmail.com>
2026-09-23 07:44:33 +0530

    Add files via upload

---
+++ Turtledemoassignment.py
@@ -0,0 +1,19 @@
+import turtle
+turtle.Screen().bgcolor ("grey")
+turtle.Screen().setup (1000, 1000)
+star = turtle.Turtle()
+star.pensize (10)
+star.pendown ()
+for i in range (4):
+    angle = 90 
+    star.forward (100)
+    star.right (angle)
+turtle.done ()
+
+
+
+
+
+
+
+
