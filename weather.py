a4f1df448ee146bab92f6b4dcd3aaa6fa635e59b
Srimaancodingal <srimaan.kv3@gmail.com>
2026-09-23 20:34:35 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,6 @@
+Spring = "It is pleasent outside "
+Autumn = "It is cold and dry outside"
+def Weather ():
+    print ("The Weather in Spring is :", Spring)
+    print ("The Weather in Autumn is :", Autumn)
+Weather ()
